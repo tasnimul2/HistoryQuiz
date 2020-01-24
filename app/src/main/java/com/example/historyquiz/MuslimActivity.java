9c162f1eb2082c562d2a0d970fe05da5dfc7f42f
Jackie <chowdhury1478@gmail.com>
2020-01-24 15:42:03 -0500

    muslimActivity added - still empty

---
+++ app/src/main/java/com/example/historyquiz/MuslimActivity.java
@@ -0,0 +1,14 @@
+package com.example.historyquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MuslimActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muslim);
+    }
+}
