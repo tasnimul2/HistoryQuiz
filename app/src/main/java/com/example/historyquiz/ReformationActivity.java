cfb1cf6a82f8a2a8f288680034a295e77d90733e
Jackie <chowdhury1478@gmail.com>
2020-01-25 12:14:15 -0500

    Europe: Protestant and Catholic Reformation acvitity added (Empty)

---
+++ app/src/main/java/com/example/historyquiz/ReformationActivity.java
@@ -0,0 +1,14 @@
+package com.example.historyquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reformation);
+    }
+}
